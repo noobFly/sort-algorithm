f5f64340f85943bb651fd5d65253b6a1cdaf27b5
noobFly <474619997@qq.com>
2016-12-13 22:45:37 +0800

    堆排序-不稳定

---
+++ TestSort.java
@@ -0,0 +1,20 @@
+/**
+ * Created by bear on 2016/2/29.
+ */
+public class TestSort {
+
+
+    public static void main(String[] args) {
+
+        new InsertionSort().print();
+        new ShellSort().print();
+        new SelectionSort().print();
+        new BubbleSort().print();
+        new MergeSort().print();
+        new QuickSort().print();
+        new HeapSort().print();
+        new RadixSort().print();
+
+
+    }
+}
