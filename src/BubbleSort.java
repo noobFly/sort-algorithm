a4e4735bd0fc60c8baabfea9e1be85c141fd6e56
noobFly <474619997@qq.com>
2017-08-02 15:22:56 +0000

    Update BubbleSort.java

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -10,7 +10,7 @@ public class BubbleSort extends AbstractSort {
         int count = 0; //已经排序过的次数
         int max_count = param.length - 1;//最大的排序次数
         while (count <= max_count) {
-            for (int i = 1; i < max_count - count; i++) {
+            for (int i = 1; i <= max_count - count; i++) {
                 if (param[i - 1] > param[i]) {
                     swap(param, i, i - 1);
                 }
@@ -20,5 +20,42 @@ public class BubbleSort extends AbstractSort {
 
         return param;
     }
+
+    public int[] sort2(int[] param) {
+        int max_count = param.length - 1;//最大下标
+        while (max_count > 0) {
+            for (int i = 0; i <= max_count - 1; i++) {
+                if (param[i] > param[i + 1]) {
+                    swap(param, i, i + 1);
+                }
+            }
+            max_count--;
+        }
+
+        return param;
+    }
+
+/*    public static void main(String[] args) {
+       int[] param = new BubbleSort().sort2(new int[]{5, 4, 3, 2, 1, 0, 44, 22, 2, 32, 54, 22, 88, 77, 99, 11, 9, 8, 7, 6, 5, 2, 6, 9, 4, 2, 5, 6, 4, 9});
+        StringBuilder str = new StringBuilder();
+        for (int i = 0; i < param.length; i++) {
+            str.append(param[i]);
+            if (i != param.length - 1) {
+                str.append(" ");
+            }
+        }
+        str.append("---------------");
+        System.out.println(str.toString());
+        int[] param2 = new BubbleSort().sort(new int[]{5, 4, 3, 2, 1, 0, 44, 22, 2, 32, 54, 22, 88, 77, 99, 11, 9, 8, 7, 6, 5, 2, 6, 9, 4, 2, 5, 6, 4, 9});
+        StringBuilder str2 = new StringBuilder();
+        for (int i = 0; i < param2.length; i++) {
+            str2.append(param2[i]);
+            if (i != param2.length - 1) {
+                str2.append(" ");
+            }
+        }
+        str2.append("---------------");
+        System.out.println(str2.toString());
+    }*/
 }
 
