4e9b645c2d47e893822604579491051e9a51a57f
xiongwenjun <xiongwenjun@utrust.cn>
2020-03-30 12:49:59 +0800

    update

--- src/main/java/com/noob/Introspector/TestIntrospector.java
+++ src/main/java/com/noob/Introspector/TestIntrospector.java
@@ -4,18 +4,27 @@ import java.beans.BeanInfo;
 import java.beans.IntrospectionException;
 import java.beans.Introspector;
 import java.beans.PropertyDescriptor;
+import java.util.function.Supplier;
 
 /**
  * 内省
  * <p>
  * PropertyDescriptor会通过解析Setter和Getter方法，合并解析结果，最终得到对应的PropertyDescriptor实例。
  * <p>
- * 除了Bean属性的之外，还会带有一个属性名为class的PropertyDescriptor实例，它的来源是Class的getClass方法，
+ * 除了Bean属性的之外，还会带有一个属性名为class的PropertyDescriptor实例，它的来源是Class的getClass方法
  * 
  * @author admin
  *
  */
 public class TestIntrospector {
+	public static String ignoreException(Supplier<String> supplier) {
+		try {
+			return supplier.get();
+		} catch (Exception e) {
+			return e.getClass().toString();
+		}
+	}
+
 	public static void main(String[] args) {
 		BeanInfo beanInfo = null;
 		try {
@@ -25,8 +34,8 @@ public class TestIntrospector {
 				String name = propertyDescriptor.getName();
 				System.out.println(name);
 				if (!"class".equals(name)) {
-					System.out.println(propertyDescriptor.getWriteMethod().getName());
-					System.out.println(propertyDescriptor.getReadMethod().getName());
+					System.out.println(ignoreException(() -> propertyDescriptor.getWriteMethod().getName()));
+					System.out.println(ignoreException(() -> propertyDescriptor.getReadMethod().getName()));
 				}
 				System.out.println("=======================");
 			}
@@ -37,13 +46,7 @@ public class TestIntrospector {
 	}
 
 	/**
-	 * 即使没有成员变量age
-	 * <p>
-	 * 有成员方法setAge： 内省结果： PropertyDescriptor.getName() 返回age。
-	 * PropertyDescriptor.getWriteMethod()返回setAge
-	 * <p>
-	 * 有成员方法getAmount： 内省结果： PropertyDescriptor.getName()
-	 * 返回amount。propertyDescriptor.getReadMethod()返回getAmount
+	 * 即使没有成员变量，但又成员方法申明就行
 	 *
 	 */
 	public static class School {
@@ -65,6 +68,10 @@ public class TestIntrospector {
 
 		public int getAmount() {
 			return 1;
+		}
+
+		public int getAddress() {
+			return this.level;
 		}
 	}
 
